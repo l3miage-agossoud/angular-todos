0de323704913daa303c3bca2906b618e1f11e510
l3miage-agossoud <romeo.agossou50@gmail>
2023-05-27 02:54:41 +0200

    update

--- karma.confi.js
+++ karma.confi.js
@@ -1,31 +1,11 @@
 module.exports = function(config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('@angular/cli/plugins/karma')
-    ],
-    client: {
-      clearContext: false // laisse le navigateur exécuter les tests dans le même contexte
-    },
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     files: [
-      { pattern: './src/test.ts', watched: false }
+      'src/**/*.spec.ts'
     ],
-    preprocessors: {
-      './src/test.ts': ['@angular/cli']
-    },
-    mime: {
-      'text/x-typescript': ['ts','tsx']
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    reporters: ['progress', 'kjhtml'],
+    singleRun: true
   });
 };
